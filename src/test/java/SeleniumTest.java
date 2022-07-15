394bbe2459b76ecc445d7f5995644acf3d294d6a
Marcin SeleniumJava <mf.kwiatkowski@wp.pl>
2022-07-15 21:42:51 +0200

    My changes

---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,15 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriver driver = new ChromeDriver();
+    }
+
+
+    public void sampleTest(){
+        System.out.println("Hello");
+    }
+}
